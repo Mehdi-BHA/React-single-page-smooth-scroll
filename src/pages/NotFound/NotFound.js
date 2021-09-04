13f83d04f02b8fe2beef2117edf08fff4797e477
Mehdi-BHA <mehdibenhadjali@gmail.com>
2021-09-04 18:57:05 +0100

    not found page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,9 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
+import HomeContainer from "../../containers/HomeContainer";
 
 const NotFound = () => {
-  return <div></div>;
+  return <HomeContainer><Typography variant="h1">NOT FOUND</Typography></HomeContainer>;
 };
 
 export default NotFound;
